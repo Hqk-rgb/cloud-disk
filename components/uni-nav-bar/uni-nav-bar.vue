f3c35334a7b9db8d7d374a682d6a287e3149bb31
OP <1332869575@qq.com>
2023-07-01 19:41:08 +0800

    自定义导航栏（二）

---
+++ components/uni-nav-bar/uni-nav-bar.vue
@@ -0,0 +1,43 @@
+<template>
+	<view>
+		<view class="fixed-top bg-main">
+			<uni-status-bar></uni-status-bar>
+			<view style="height: 44px;" class="flex border-bottom align-center">
+				<view class="flex-1 flex">
+					<!-- 具名插槽 -->
+					<slot name="left"></slot>
+				</view>
+				<view class="flex-1 flex justify-center">
+					<!-- 默认插槽 -->
+					<slot />
+				</view>
+				<view class="flex-1 flex justify-end">
+					<!-- 具名插槽 -->
+					<slot name="right"></slot>
+				</view>
+			</view>
+		</view>
+		<!-- 占位 -->
+		<view>
+			<uni-status-bar></uni-status-bar>
+			<view style="height: 44px;"></view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,30 +1,27 @@
 <template>
 	<view>
 		<!-- 自定义导航栏 -->
-		<view>
-			<!-- 引入状态栏组件 -->
-			<uni-status-bar></uni-status-bar>
-			<!-- 自定义导航栏分成左中右三部分，用flex布局，每个各占一份 -->
-			<view style="height: 44px;" class="flex bottom-border align-center bg-main">
-				<!-- 左 -->
-				<view class="flex-1 flex">
-					<text class="ml-2 text-light">首页</text>
-				</view>
-				<!-- 中 -->
-				<view class="flex-1 flex justify-center"></view>
-				<!-- 右 -->
-				<view class="flex-1 flex justify-end">
-					<view style="width: 60rpx;height: 60rpx;" class="flex align-center justify-center bg-light rounded-circle mr-3">
-						<text class="iconfont icon-zengjia"></text>
-					</view>
-					<view style="width: 60rpx;height: 60rpx;" class="flex align-center justify-center bg-light rounded-circle mr-3">
-						<text class="iconfont icon-gengduo"></text>
-					</view>
-				</view>
+		<uni-nav-bar>
+		<!-- left插槽 -->
+		<template v-slot:left>
+			<text class="ml-3 text-light font-md">首页</text>
+		</template>
+		<!-- right插槽 -->
+		<template v-slot:right>
+			<view style="width: 60rpx;height: 60rpx;"
+				class="flex align-center justify-center bg-light rounded-circle mr-3">
+				<text class="iconfont icon-zengjia"></text>
 			</view>
-		</view>
+			<view style="width: 60rpx;height: 60rpx;"
+				class="flex align-center justify-center bg-light rounded-circle mr-3">
+				<text class="iconfont icon-gengduo"></text>
+			</view>
+
+		</template>
+</uni-nav-bar>
 	</view>
 </template>
+
 
 <script>
 	export default {
