32aa6914664bdebbad6e28675bd0874472359c29
OP <1332869575@qq.com>
2023-07-01 19:24:01 +0800

    自定义导航栏（一)

---
+++ components/uni-status-bar/uni-status-bar.vue
@@ -0,0 +1,23 @@
+<template>
+  <view :style="{ height: statusBarHeight }" class="uni-status-bar bg-primary">
+    <slot />
+  </view>
+</template>
+
+<script>
+export default {
+  name: "UniStatusBar",
+  data() {
+    return {
+      statusBarHeight: uni.getSystemInfoSync().statusBarHeight + "px",
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.uni-status-bar {
+  // height: 20px;
+  height: var(--status-bar-height);
+}
+</style>
